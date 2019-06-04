d911961781849a2faf0a80d6c0c29eaed3fc7bc1
Jonas <Jonas@192.168.1.9>
2019-06-04 14:05:29 -0300

    1 tela

---
+++ ProjetoPizzaria/src/Gui/TelaInicial.java
@@ -0,0 +1,78 @@
+package Gui;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+
+public class TelaInicial extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_1;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					TelaInicial frame = new TelaInicial();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public TelaInicial() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 400);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblPizzariaRural = new JLabel("\t\t            Pizzaria Rural");
+		lblPizzariaRural.setFont(new Font("Tahoma", Font.BOLD, 29));
+		lblPizzariaRural.setBounds(10, 11, 414, 60);
+		contentPane.add(lblPizzariaRural);
+		
+		JLabel lblUsurio = new JLabel("Usu\u00E1rio:");
+		lblUsurio.setFont(new Font("Tahoma", Font.BOLD, 15));
+		lblUsurio.setBounds(50, 129, 81, 36);
+		contentPane.add(lblUsurio);
+		
+		textField = new JTextField();
+		textField.setBounds(136, 139, 139, 26);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblSenha = new JLabel("Senha:");
+		lblSenha.setFont(new Font("Tahoma", Font.BOLD, 15));
+		lblSenha.setBounds(50, 190, 81, 26);
+		contentPane.add(lblSenha);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(136, 192, 139, 26);
+		contentPane.add(textField_1);
+		textField_1.setColumns(10);
+		
+		JButton btnEntrar = new JButton("ENTRAR");
+		btnEntrar.setFont(new Font("Tahoma", Font.BOLD, 16));
+		btnEntrar.setBounds(164, 275, 111, 23);
+		contentPane.add(btnEntrar);
+	}
+}
